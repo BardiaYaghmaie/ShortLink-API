4ac25ece96f4baa2cd367f9173839b51797aa1ba
BardiaYaghmaie <bardia_yaghmaie@mathdep.iust.ac.ir>
2023-07-15 10:41:28 +0330

    app creates the tables when running

--- app/database.py
+++ app/database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from .models import Base
 
 # Database setup - local
-engine = create_engine("postgresql://postgres:1qaz2wsx@localhost/shortlinks_DBcontext")
+engine = create_engine("postgresql://postgres:1qaz2wsx@shortlink-api-db-1/shortlinks_DBcontext")
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 def get_db():
@@ -11,3 +12,6 @@ def get_db():
         yield db
     finally:
         db.close()
+
+Base.metadata.create_all(bind=engine)
+
